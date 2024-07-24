da5c9703af7aad3242e2352eae93587c991c7dc4
Jeongeunju <laco3127@gmail.com>
2024-07-24 16:24:30 +0900

    [#1] Header 기능 개발

--- goodsending/src/App.jsx
+++ goodsending/src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Button>Click me</Button>
+      <Header />
     </div>
   );
 }
---
+++ goodsending/src/components/Header.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import { useState } from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "./ui/button";
+// import BlackLogo from "@/assets/icon/BlackLogo.png";
+
+const Header = () => {
+  const [isLogIn, setIsLogIn] = useState(false);
+
+  const [Cash, setCash] = useState(10000);
+
+  const userName = "사용자 이름";
+
+  return (
+    <header className="fixed top-0 left-0 right-0 flex justify-between items-center p-4 bg-white">
+      <div className="flex items-center">
+        <img src="../public/icon/LogoBlack.png" alt="logo" className="h-8" />
+      </div>
+
+      <nav className="flex items-center space-x-4">
+        {isLogIn ? (
+          <>
+            <Badge className="bg-green-200 text-green-800 px-2 py-1 rounded-full">
+              잔여 캐시: {Cash}원
+            </Badge>
+            <button>판매/등록</button>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger>마이페이지</DropdownMenuTrigger>
+
+              <DropdownMenuContent>
+                <DropdownMenuItem>회원 정보 수정</DropdownMenuItem>
+
+                <DropdownMenuItem>쿠폰함</DropdownMenuItem>
+
+                <DropdownMenuItem>찜한 상품</DropdownMenuItem>
+
+                <DropdownMenuItem>캐시 충전</DropdownMenuItem>
+
+                <DropdownMenuItem>경매신청 내역</DropdownMenuItem>
+
+                <DropdownMenuItem>경매판매 내역</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <span>고객센터</span>
+
+            <span>{userName}</span>
+          </>
+        ) : (
+          <>
+            <span>고객센터</span>
+
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              onClick={() => setIsLogIn(true)}
+            >
+              로그인
+            </button>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
