7990f2274882f88364a25ed76db5adca56aa8e31
RYUJINSIK <it_jinsik@naver.com>
2024-07-27 04:00:30 +0900

    [#5] : tailwind 색상 변수 지정

--- goodsending/tailwind.config.js
+++ goodsending/tailwind.config.js
@@ -3,7 +3,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3490dc",
+          light: "#6cb2eb",
+          dark: "#2779bd",
+        },
+        secondary: {
+          DEFAULT: "#ffed4a",
+          light: "#fff382",
+          dark: "#e3ac00",
+        },
+        // 추가 색상 정의
+      },
+    },
   },
   plugins: [],
 };
