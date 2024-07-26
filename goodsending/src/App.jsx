828b818b07c3c8f319961fd5779bf2c8a3afd740
Jeongeunju <laco3127@gmail.com>
2024-07-27 04:35:58 +0900

    [#4] 메인기능 개발

--- goodsending/src/App.jsx
+++ goodsending/src/App.jsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Header from "./components/Header";
 import "./App.css";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
       <Button>Click me</Button>
       <Header />
+      <Main />
     </div>
   );
 }
