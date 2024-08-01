91c17697758959c328162551894feba9b0c35f2e
RYUJINSIK <it_jinsik@naver.com>
2024-08-02 02:24:43 +0900

    [#2] : 회원가입 화면 , 폼 구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Main from "./pages/Main";
-import SignUp from "./pages/SignUp";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,87 @@
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { KeyRound } from "lucide-react";
 
 function SignUp() {
+  const [email, setEmail] = useState(""); // 가입메일
+  const [code, setCode] = useState(""); // 인증코드
+  const [showCodeField, setShowCodeField] = useState(false); // 인증코드입력칸 제어
+  const [mailError, setMailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const getCode = () => {
+    // 이메일인증 버튼
+    if (validateEmail(email)) {
+      setShowCodeField(true);
+    } else {
+      setMailError("메일주소가 잘못되었습니다.");
+      setShowCodeField(false);
+    }
+  };
+  const labelStyle = "flex w-full max-w-sm items-start pr-2 pb-2 pt-2 text-sm";
+  const errorStyle = "flex w-full max-w-sm items-start text-sm text-primary";
   return (
-    <div>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" placeholder="Email" />
-        <Button type="submit">Subscribe</Button>
+    <div className="w-full">
+      <img src="../icon/LogoBlue.png" alt="logo" className="h-20" />
+      <p className={labelStyle}>이메일</p>
+      <div className="flex w-full max-w-sm items-center space-x-2 pb-2">
+        <Input
+          type="text"
+          placeholder="ex ) goods@ending.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button type="button" onClick={getCode}>
+          인증번호 받기
+        </Button>
+      </div>
+      {mailError && <p className={errorStyle}>{mailError}</p>}
+
+      {/* <div className="flex w-full items-center"> */}
+      <div
+        className={`transition-all duration-300 ease-in-out overflow-hidden ${
+          showCodeField ? "max-h-20 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="flex  items-start">
+          <p className={labelStyle}>인증번호 입력</p>
+          <InputOTP
+            maxLength={6}
+            pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+            value={code}
+            onChange={(value) => setCode(value)}
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+              <InputOTPSlot index={3} />
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+        </div>
+      </div>
+      <p className={labelStyle}>비밀번호</p>
+      <Input type="password" placeholder="**********" />
+      <p className={labelStyle}>비밀번호 확인</p>
+      <Input type="password" placeholder="**********" />
+      {mailError && <p className={errorStyle}>{mailError}</p>}
+      <div className="flex w-full max-w-sm items-center space-x-2 pt-3">
+        <Button className="w-full">
+          <KeyRound className="mr-2 h-4 w-4" /> 가입하기
+        </Button>
       </div>
     </div>
   );
